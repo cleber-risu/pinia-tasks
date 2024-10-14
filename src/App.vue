0a3b4bd0b49f071014a890d4ec55f3f509548f00
cleber <cleber.risu@outlook.com>
2024-10-14 13:15:04 -0300

    add style

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,13 @@
 </script>
 
 <template>
-
+  <main>
+    <!-- heading -->
+    <header>
+      <img src="./assets/pinia-logo.svg" alt="Pinia logo" />
+      <h1>Pinia tasks</h1>
+    </header>
+  </main>
 </template>
 
 <style scoped>
