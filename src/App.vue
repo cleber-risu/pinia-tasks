2c56f78badef0a0f5f6f551d6820c4573d568b11
cleber <cleber.risu@outlook.com>
2024-10-15 13:02:44 -0300

    add more async actions

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <script setup>
 import { ref } from 'vue';
+import { storeToRefs } from 'pinia';
 
 import { useTaskStore } from './stores/TaskStore';
 import TaskDetails from './components/TaskDetails.vue';
 import TaskForm from './components/TaskForm.vue';
 
 const taskStore = useTaskStore()
+
+const { tasks, isLoading, favs, totalCount, favCount } = storeToRefs(taskStore)
 
 // fetch tasks
 taskStore.getTasks()
@@ -21,11 +24,6 @@ const filter = ref('all')
       <h1>Pinia tasks</h1>
     </header>
 
-    <!-- loading -->
-    <div class="loading" v-if="taskStore.isLoading">
-      Loading Tasks...
-    </div>
-
     <!-- new task form -->
      <div class="new-task-form">
       <TaskForm />
@@ -37,20 +35,27 @@ const filter = ref('all')
       <button @click="filter = 'favs'">Fav tasks</button>
     </nav>
 
+    <!-- loading -->
+    <div class="loading" v-if="isLoading">
+      Loading Tasks...
+    </div>
+
     <!-- task list -->
     <div class="task-list" v-if="filter === 'all'">
-      <p>You have <strong>{{ taskStore.totalCount }}</strong> tasks left to do</p>
-      <div v-for="task in taskStore.tasks" :key="task.id">
+      <p>You have <strong>{{ totalCount }}</strong> tasks left to do</p>
+      <div v-for="task in tasks" :key="task.id">
         <TaskDetails :task="task" />
       </div>
     </div>
      
     <div class="task-list" v-if="filter === 'favs'">
-      <p>You have <strong>{{ taskStore.favCount }}</strong> tasks left to do</p>
-      <div v-for="task in taskStore.favs" :key="task.id">
+      <p>You have <strong>{{ favCount }}</strong> tasks left to do</p>
+      <div v-for="task in favs" :key="task.id">
         <TaskDetails :task="task" />
       </div>
     </div>
+
+    <button @click="taskStore.$reset">Reset</button>
   </main>
 </template>
 
